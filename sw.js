001e8e32715f8fccce584f9cef01f090423e4126
Melina Markstein <106178084+mcmarkstein@users.noreply.github.com>
2026-04-02 22:09:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
 
+const CACHE_NAME = 'fundida-app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// Instalar el service worker y cachear el contenido básico
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Responder con recursos en cache si no hay internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
